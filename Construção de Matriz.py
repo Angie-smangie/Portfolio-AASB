730c7dbedf86e88a65e68080b4225039af211d7b
Angie-smangie <angela.sousa.anoaqui@gmail.com>
2025-12-26 17:31:51 +0000

    Update Construção de Matriz.py

--- Construção de Matriz.py
+++ Construção de Matriz.py
@@ -2,7 +2,8 @@ def matriz_de_zeros(seq1,seq2):
     matriz_a_zeros=[[0 for n2 in range(len(seq2)+1)] for n1 in range(len(seq1)+1)]
     return matriz_a_zeros
 
-def primeira_linha_e_coluna(matriz,seq1,seq2,space):
+def primeira_linha_e_coluna(seq1,seq2,space):
+    matriz=matriz_de_zeros(seq1,seq2)
     for p1 in range(len(seq1)):
         matriz[0][p1+1]=matriz[0][p1]+space
     for p2 in range(len(seq2)):
@@ -10,8 +11,4 @@ def primeira_linha_e_coluna(matriz,seq1,seq2,space):
     return matriz
 
 def needleman_wunsch(seq1,seq2,match=2,mismatch=-2,space=-5):
-    matriz=matriz_de_zeros(seq1,seq2)
-    primeira_linha_e_coluna(matriz,seq1,seq2,space)
-    return primeira_linha_e_coluna
-
-needleman_wunsch("SKJD","SDKDJSOIDF")
+    return primeira_linha_e_coluna(matriz,seq1,seq2,space)
