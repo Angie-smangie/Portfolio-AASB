cd86a4592ad9658c791d37e33ae54f00ddaac79b
Angie-smangie <angela.sousa.anoaqui@gmail.com>
2025-12-26 17:26:21 +0000

    Update Construção de Matriz.py
    ctrl c , ctrl v

--- Construção de Matriz.py
+++ Construção de Matriz.py
@@ -1,65 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 70,
-   "id": "f7c3874f",
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "IndexError",
-     "evalue": "list index out of range",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[31m---------------------------------------------------------------------------\u001b[39m",
-      "\u001b[31mIndexError\u001b[39m                                Traceback (most recent call last)",
-      "\u001b[36mCell\u001b[39m\u001b[36m \u001b[39m\u001b[32mIn[70]\u001b[39m\u001b[32m, line 17\u001b[39m\n\u001b[32m     14\u001b[39m     primeira_linha_e_coluna(matriz,seq1,seq2,space)\n\u001b[32m     15\u001b[39m     \u001b[38;5;28;01mreturn\u001b[39;00m primeira_linha_e_coluna\n\u001b[32m---> \u001b[39m\u001b[32m17\u001b[39m \u001b[43mneedleman_wunsch\u001b[49m\u001b[43m(\u001b[49m\u001b[33;43m\"\u001b[39;49m\u001b[33;43mSKJD\u001b[39;49m\u001b[33;43m\"\u001b[39;49m\u001b[43m,\u001b[49m\u001b[33;43m\"\u001b[39;49m\u001b[33;43mSDKDJSOIDF\u001b[39;49m\u001b[33;43m\"\u001b[39;49m\u001b[43m)\u001b[49m\n",
-      "\u001b[36mCell\u001b[39m\u001b[36m \u001b[39m\u001b[32mIn[70]\u001b[39m\u001b[32m, line 14\u001b[39m, in \u001b[36mneedleman_wunsch\u001b[39m\u001b[34m(seq1, seq2, match, mismatch, space)\u001b[39m\n\u001b[32m     12\u001b[39m \u001b[38;5;28;01mdef\u001b[39;00m\u001b[38;5;250m \u001b[39m\u001b[34mneedleman_wunsch\u001b[39m(seq1,seq2,match=\u001b[32m2\u001b[39m,mismatch=-\u001b[32m2\u001b[39m,space=-\u001b[32m5\u001b[39m):\n\u001b[32m     13\u001b[39m     matriz=matriz_de_zeros(seq1,seq2)\n\u001b[32m---> \u001b[39m\u001b[32m14\u001b[39m     \u001b[43mprimeira_linha_e_coluna\u001b[49m\u001b[43m(\u001b[49m\u001b[43mmatriz\u001b[49m\u001b[43m,\u001b[49m\u001b[43mseq1\u001b[49m\u001b[43m,\u001b[49m\u001b[43mseq2\u001b[49m\u001b[43m,\u001b[49m\u001b[43mspace\u001b[49m\u001b[43m)\u001b[49m\n\u001b[32m     15\u001b[39m     \u001b[38;5;28;01mreturn\u001b[39;00m primeira_linha_e_coluna\n",
-      "\u001b[36mCell\u001b[39m\u001b[36m \u001b[39m\u001b[32mIn[70]\u001b[39m\u001b[32m, line 9\u001b[39m, in \u001b[36mprimeira_linha_e_coluna\u001b[39m\u001b[34m(matriz, seq1, seq2, space)\u001b[39m\n\u001b[32m      7\u001b[39m     matriz[\u001b[32m0\u001b[39m][p1+\u001b[32m1\u001b[39m]=matriz[\u001b[32m0\u001b[39m][p1]+space\n\u001b[32m      8\u001b[39m \u001b[38;5;28;01mfor\u001b[39;00m p2 \u001b[38;5;129;01min\u001b[39;00m \u001b[38;5;28mrange\u001b[39m(\u001b[38;5;28mlen\u001b[39m(seq2)):\n\u001b[32m----> \u001b[39m\u001b[32m9\u001b[39m     \u001b[43mmatriz\u001b[49m\u001b[43m[\u001b[49m\u001b[43mp2\u001b[49m\u001b[43m+\u001b[49m\u001b[32;43m1\u001b[39;49m\u001b[43m]\u001b[49m[\u001b[32m0\u001b[39m]=matriz[p2][\u001b[32m0\u001b[39m]+space\n\u001b[32m     10\u001b[39m \u001b[38;5;28;01mreturn\u001b[39;00m matriz\n",
-      "\u001b[31mIndexError\u001b[39m: list index out of range"
-     ]
-    }
-   ],
-   "source": [
-    "def matriz_de_zeros(seq1,seq2):\n",
-    "    matriz_a_zeros=[[0 for n2 in range(len(seq2)+1)] for n1 in range(len(seq1)+1)]\n",
-    "    return matriz_a_zeros\n",
-    "\n",
-    "def primeira_linha_e_coluna(matriz,seq1,seq2,space):\n",
-    "    for p1 in range(len(seq1)):\n",
-    "        matriz[0][p1+1]=matriz[0][p1]+space\n",
-    "    for p2 in range(len(seq2)):\n",
-    "        matriz[p2+1][0]=matriz[p2][0]+space\n",
-    "    return matriz\n",
-    "\n",
-    "def needleman_wunsch(seq1,seq2,match=2,mismatch=-2,space=-5):\n",
-    "    matriz=matriz_de_zeros(seq1,seq2)\n",
-    "    primeira_linha_e_coluna(matriz,seq1,seq2,space)\n",
-    "    return primeira_linha_e_coluna\n",
-    "\n",
-    "needleman_wunsch(\"SKJD\",\"SDKDJSOIDF\")"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "aasb",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.10"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+def matriz_de_zeros(seq1,seq2):
+    matriz_a_zeros=[[0 for n2 in range(len(seq2)+1)] for n1 in range(len(seq1)+1)]
+    return matriz_a_zeros
+
+def primeira_linha_e_coluna(matriz,seq1,seq2,space):
+    for p1 in range(len(seq1)):
+        matriz[0][p1+1]=matriz[0][p1]+space
+    for p2 in range(len(seq2)):
+        matriz[p2+1][0]=matriz[p2][0]+space
+    return matriz
+
+def needleman_wunsch(seq1,seq2,match=2,mismatch=-2,space=-5):
+    matriz=matriz_de_zeros(seq1,seq2)
+    primeira_linha_e_coluna(matriz,seq1,seq2,space)
+    return primeira_linha_e_coluna
+
+needleman_wunsch("SKJD","SDKDJSOIDF")
